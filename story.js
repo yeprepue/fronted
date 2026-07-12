58c1f7a7d2053fc95ef182390bd769221401bfbf
Yeison Esteban Pretel Puentes <yeisonpretel71@gmail.com>
2026-07-12 00:36:17 -0500

    refactor(logic): Mejorar el desarrollo de la historia y añadir personalización para el usuario
    Reestructurar el motor de la historia para que admita mensajes finales condicionales en función
    del «happyScore» acumulado e implementar un mecanismo de reinicio. Se ha añadido
    la funcionalidad de introducción del nombre de usuario y se ha actualizado la estructura de datos de la historia para incluir
    estados finales ramificados.
    
    - Implementar la lógica de `finalMessage` para devolver resultados positivos, neutros o negativos
      en función de la puntuación
    - Añadir la funcionalidad `reset` al `clickHandler` para reiniciar la historia
    - Integrar la introducción del nombre de usuario mediante `bind:value` en `App.svelte`
    - Actualizar `story.js` con nuevos diálogos, valores de botones y objetos de estado final
    
    - Añadir estilo al campo de introducción del nombre y la lógica de visibilidad del encabezado

--- story.js
+++ story.js
@@ -1,22 +1,48 @@
 export default [
     {
-        smileySays: '¡Hola! ¿Cómo estás hoy?',
+        smileySays: '¡Hola! ¿Cómo estás?',
         buttons: [
-            { text: 'Bien 🙂', value: 1 },
-            { text: 'Mal 🙁', value: -1 }
+            { value: -1, text: '¡No es asunto tuyo!' },
+            { value: 0, text: 'Bien, gracias.' }
         ]
     },
     {
-        smileySays: 'Cuéntame algo divertido.',
+        smileySays: '¿Cuánta madera podría cortar una marmota si una marmota pudiera cortar madera?',
         buttons: [
-            { text: 'Un chiste', value: 1 },
-            { text: 'No quiero', value: 0 }
+            { value: 1, text: '¡Vaya, qué buena pregunta! :D' },
+            { value: 0, text: '¿4..?' }
         ]
     },
     {
-        smileySays: '¡Gracias por jugar!',
+        smileySays: '¿Cuál es mejor, Svelte o React?',
         buttons: [
-            { text: 'Otra vez', value: 1 }
+            { value: 2, text: '¡Svelte es el futuro!' },
+            { value: -1, text: 'React por siempreeeeeee' }
+        ]
+    },
+    {
+        smileySays: '¿Quieres escuchar la última pregunta?',
+        buttons: [
+            { value: -1, text: 'No..' },
+            { value: 1, text: '¡Sí!' }
+        ]
+    },
+    {
+        smileySays: '¿Me quieres?',
+        buttons: [
+            { value: 0, text: 'emmmmmm......' },
+            { value: 1, text: '¡Claro que sí!' },
+            { value: -2, text: 'Definitivamente te odio.' }
+        ]
+    },
+    {
+        end: {
+            nice: 'Creo que a Smiley le caes bien.',
+            neutral: 'No sé si a Smiley le caes bien..',
+            mean: 'A Smiley no le caes bien..',
+        },
+        buttons: [
+            { value: 'reset', text: 'Reiniciar' }
         ]
     }
 ];
